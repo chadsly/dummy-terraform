50efc41993d898ccdf1b437ba5740ffdbf788d4e
Chad <chad@expansiagroup.com>
2020-04-19 21:36:03 -0400

    make private key output

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "server_hostnames" {
 output "server_keys" {
   value = "${aws_key_pair.generated_key.key_name}"
 }
+
+output "private_key" {
+  value = "${tls_private_key.example.private_key_pem}"
+}
